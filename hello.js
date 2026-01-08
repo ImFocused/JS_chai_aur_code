07425bbdf0e6c673bd2131e1848bb41777fcd97d
ImFocused <prathamjainpbj2306@gmail.com>
2026-01-08 11:19:57 +0000

    till data types, conversion

--- hello.js
+++ hello.js
@@ -1,1 +1,13 @@
+"use strict" // used to treat all js code as newer version
 console.log("hello!!!")
+let age =18
+let name ="pro"
+console.log(typeof name)
+console.log(typeof null) // object 
+
+let ageinstr = Number(age); // String , Boolean 
+console.log(ageinstr);
+console.log(typeof ageinstr); // if value is not number then it gives NaN ! for all except null
+// Null is too converted to 0 from str to number 
+
+// Empty string to boolean is False and others is true (for strings)
