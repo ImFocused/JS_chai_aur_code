c5134d06e0a574b8fb7016dbf8981e88c440421a
ImFocused <prathamjainpbj2306@gmail.com>
2026-01-11 05:54:52 +0000

    null , defined , bigint

--- hello.js
+++ hello.js
@@ -11,3 +11,14 @@ console.log(typeof ageinstr); // if value is not number then it gives NaN ! for 
 // Null is too converted to 0 from str to number 
 
 // Empty string to boolean is False and others is true (for strings)
+
+console.log("2"===2) // === strict checks value as well as datatype
+console.log("2"==2)
+
+console.log(null == 0) // but equality operator works different
+console.log(null < 0) 
+console.log(null <= 0)  // Null here be referred as zero and when it checks with zero its true
+
+// and UNDEFINED gives false for all except NULL == UNDEFINED 
+
+// BigInt data type is used by using n at last of number to be input
