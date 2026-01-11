e4c56a98b1b5ed1a597b7cda6dbed48e2ea6cdb2
ImFocused <prathamjainpbj2306@gmail.com>
2026-01-11 06:22:12 +0000

    new file abt memory

---
+++ memory.js
@@ -0,0 +1,5 @@
+// Stack mem is used by primitive datatypes 
+// heap from non primitive datatypes like Arrays , object , functions 
+
+// stack memory gives a copy 
+// heap gives the reference to the original file that will be edited
