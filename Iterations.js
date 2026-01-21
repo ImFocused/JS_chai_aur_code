f894fb4d64ef428eb118f559955400403ba8ee8f
ImFocused <prathamjainpbj2306@gmail.com>
2026-01-21 04:06:48 +0000

    loops for , while

---
+++ Iterations.js
@@ -0,0 +1,24 @@
+for (let i = 0; i < 10; i++) {
+    const element = i;
+
+    console.log(element);
+    
+}
+
+// for(i=1;i<=10;i++){
+//     for(j=1;j<=10;j++){
+//     console.log(i + '*' + j + '=' + (i*j))
+//     }
+// }
+
+// bREAK is used for getting out of the whole block 
+// CONTINUE is like ek baar maaf krdo and continues the other iterations 
+
+myarr = ['Spiderman' , 'Batman' , 'ironman']
+let i = 0 ;
+
+while(i < myarr.length){
+    console.log(myarr[i]);
+    i+=2;
+
+}
