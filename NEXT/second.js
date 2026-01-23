2524192e0f96a185e78d5f9f98a663c400c95f53
ImFocused <prathamjainpbj2306@gmail.com>
2026-01-23 17:49:39 +0000

    creating nodes

--- NEXT/second.js
+++ NEXT/second.js
@@ -1,9 +1,10 @@
 const div = document.createElement('div');
 div.className = "main";
 div.id = Math.round(Math.random()*10);
-div.innertext = "HELLO DIV ";
+div.innerText = "HELLO DIV ";
 div.setAttribute("title","this is a div");
 console.log(div);
 div.style.backgroundColor = "lightblue";
+div.style.color = "black";
 div.style.padding = "10px";
-document.body.appendChild(div);
+document.body.appendChild(div);  // have too append it at last as it was stored in memory only first
