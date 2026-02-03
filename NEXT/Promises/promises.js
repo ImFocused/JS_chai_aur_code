a63249c6b4bcfe42565e973e07b50d150fe4f876
ImFocused <prathamjainpbj2306@gmail.com>
2026-02-03 18:36:02 +0000

    fetch in promises 2

--- NEXT/Promises/promises.js
+++ NEXT/Promises/promises.js
@@ -17,3 +17,31 @@ promise1.then(function(){  // promise consumed using then keyword
 })
 
 console.log(typeof promise1)
+
+
+const promise2 = new Promise(function(resolve,reject){  
+  
+
+    setTimeout(function(){
+        console.log("async task is completed!");
+        resolve({"name": "pro", "email" : "pratham@example.com"});  // using resolve the data is passed too in the form of objects 
+    },1000);
+
+})
+
+promise2.then(function(user){  // promise consumed using then keyword
+    console.log(user)
+})
+
+
+// .catch for reject
+// .finally that gets executed always 
+
+fetch("https://api.github.com/users/ImFocused")
+.then((response)=>{
+    return response.json();   // converts it in object format for readability json also returns promise
+})
+.then((data)=>{
+    console.log(data);
+})
+.catch((error)=>console.log(error))
